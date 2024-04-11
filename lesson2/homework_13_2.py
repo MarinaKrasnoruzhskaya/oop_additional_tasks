25182102b4c729fb305c194753993a5596e35e3c
Marina Krasnoruzhskaya <krasnoruzhskayamarina@yandex.ru>
2024-04-11 15:25:43 +0400

    solution homework 13.2 (lesson 2)

---
+++ lesson2/homework_13_2.py
@@ -0,0 +1,165 @@
+import warnings
+
+warnings.filterwarnings('ignore')
+
+
+class Product:
+    """
+    Класс для описания товара в магазине
+    """
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.__price = price
+        self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product):
+        return cls(**product)
+
+    @property
+    def price(self):
+        return  self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
+
+
+class Category:
+    """
+    Класс для категорий товара
+    """
+    name: str
+    description: str
+    products: list
+
+    product_count = 0
+    category_count = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.__products = products
+        Category.category_count += 1
+        Category.product_count += len(self.products)
+
+    @property
+    def products(self):
+        products_str = ''
+        for product in self.__products:
+            products_str += f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n'
+        return products_str
+
+
+data = [
+    {
+        "name": "Смартфоны",
+        "description": "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
+        "products": [
+            {
+                "name": "Samsung Galaxy C23 Ultra",
+                "description": "256GB, Серый цвет, 200MP камера",
+                "price": 180000.0,
+                "quantity": 5
+            },
+            {
+                "name": "Iphone 15",
+                "description": "512GB, Gray space",
+                "price": 210000.0,
+                "quantity": 8
+            },
+            {
+                "name": "Xiaomi Redmi Note 11",
+                "description": "1024GB, Синий",
+                "price": 31000.0,
+                "quantity": 14
+            }
+        ]
+    },
+    {
+        "name": "Телевизоры",
+        "description": "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        "products": [
+            {
+                "name": "55 QLED 4K",
+                "description": "Фоновая подсветка",
+                "price": 123000.0,
+                "quantity": 7
+            }
+        ]
+    }
+]
+
+categories = []
+for category in data:
+    products = []
+    for product in category['products']:
+        products.append(Product.new_product(product))
+    category['products'] = products
+    categories.append(Category(**category))
+
+assert categories[0].products == '''Samsung Galaxy C23 Ultra, 180000.0 руб. Остаток: 5 шт.
+Iphone 15, 210000.0 руб. Остаток: 8 шт.
+Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.
+'''
+# 1
+product_item = Product('Test', 'Test', 1000, 10)
+print(product_item.price)
+
+product_item.price = 800
+print(product_item.price)
+# Ожидаемый ответ:
+# 1000
+# 800
+
+# 2 +
+product_item = Product('Test', 'Test', 1000, 10)
+print(product_item.price)
+
+
+product_item.price = -100
+print(product_item.price)
+
+# Ожидаемый ответ:
+# 1000
+# Цена не должна быть нулевая или отрицательная
+# 1000
+
+# 3 +
+product_data = {
+    'name': 'New Product',
+    'description': 'New Description',
+    'price': 500,
+    'quantity': 5
+}
+
+new_product = Product.new_product(product_data)
+print(new_product.name)
+print(new_product.description)
+print(new_product.price)
+print(new_product.quantity)
+# Ожидаемый ответ:
+#
+# New Product
+# New Description
+# 500
+# 5
+
+# 4 +
+print( categories[0].products == '''Samsung Galaxy C23 Ultra, 180000.0 руб. Остаток: 5 шт.
+Iphone 15, 210000.0 руб. Остаток: 8 шт.
+Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.
+''')
+#
+# Ожидаемый ответ:
+# True
+
